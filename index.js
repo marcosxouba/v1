72cdbc13cc860aaea7ac136fae4a599a9a49c69a
marcosxouba <marcosxouba@gmail.com>
2021-04-02 16:10:45 +0200

    Update index.js

--- index.js
+++ index.js
@@ -651,6 +651,8 @@ _*Recuerda leer las normas de la descripción.*_
   ┃ _Lista de inactivos_
   ┠⊷️ *${prefix}antilink*
   ┃ _AntiSpam_
+  ┠⊷️ *${prefix}debounce*
+  ┃ _Update_
   ┗━━━━━━━━━━━━━━━━━━━
   *〘 DIVERSIÓN 〙*
   ┏━━━━━━━━━━━━━━━━━━━
@@ -3355,6 +3357,41 @@ module.exports = handler
   mentions('〘  *TAGALL* 〙\n┏━━━━━━━━━━━━━━━━\n┠⊷'+teks+'┃━━━━━━━━━━\n┃────ACTIVOS 24/7────\n┗━━━━━━━━━━━━━━━', members_id, true)
   break
   
+	//---Restart				  
+					  
+	let { spawn }  = require('child_process');
+let handler  = async (m, { conn }) => {
+  if (!process.send) throw 'Dont: node main.js\nDo: node index.js'
+  if (global.conn.user.jid == conn.user.jid) {
+    await m.reply('Reiniciando Bot...\nEspere aproximadamente 1 minuto')
+    await global.DATABASE.save()
+    process.send('reset')
+  } else throw '_eeeeeiiittsssss..._'
+}
+handler.help = ['debounce' + (process.send ? '' : ' (Not working)')]
+handler.tags = ['host']
+handler.command = /^debounce$/i
+handler.owner = true
+handler.mods = false
+handler.premium = false
+handler.group = false
+handler.private = false
+
+handler.admin = false
+handler.botAdmin = false
+
+handler.fail = null
+
+module.exports = handler				  
+					  
+					  
+ })
+					  
+  break		  
+					  
+					  
+					  
+					  
   
   //clear all chat
 				  case 'clearall':
